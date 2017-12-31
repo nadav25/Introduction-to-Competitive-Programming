f452f62e02fce1d910cfef9e68dfdfaa02f7d74a
nadav25 <nadovolo@gmail.com>
2017-12-31 21:03:00 +0200

    main
    Separation main

--- Competitive-Programming/CompetitiveProgramming/CompetitiveProgramming/PracticeArrays.cs
+++ Competitive-Programming/CompetitiveProgramming/CompetitiveProgramming/PracticeArrays.cs
@@ -6,20 +6,6 @@ using System.Threading.Tasks;
 
 namespace CompetitiveProgramming
 {
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Question 1");
-            int[] ArrayForExample = { 2, 3, 1, 0, 2, 5, 3, 8, 7 };
-            ArryQuestion1 cheek = new ArryQuestion1();
-            int DuplecateNumber = cheek.Duplecate(ArrayForExample);
-            Console.WriteLine("the duplicate number is : " + DuplecateNumber );
-        }
-    }
-
-
     /// <summary>
     /// An array contains n numbers ranging from 0 to n-1. There are some numbers duplicated in the
     /// array.It is not clear how many numbers are duplicated or how many times a number gets duplicated.How do you
@@ -34,7 +20,7 @@ namespace CompetitiveProgramming
     {
         public int Duplecate(int[] i_Arry)
         {
-            int DuplecateNumber = -1;  //If there is no duplicate number, it will be returned -1
+            int DuplecateNumber = -1;  //If there is no duplicate number, it will returned -1
             for (int i = 0; i < i_Arry.Length; i++)
             {
                 while (i_Arry[i] != i)
--- Competitive-Programming/CompetitiveProgramming/CompetitiveProgramming/Program.cs
+++ Competitive-Programming/CompetitiveProgramming/CompetitiveProgramming/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace CompetitiveProgramming
 {
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //    }
-    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Question 1");
+            int[] ArrayForExample = { 2, 3, 1, 0, 2, 5, 3, 8, 7 };
+            ArryQuestion1 cheek = new ArryQuestion1();
+            int DuplecateNumber = cheek.Duplecate(ArrayForExample);
+            Console.WriteLine("the duplicate number is : " + DuplecateNumber);
+        }
+    }
 }
